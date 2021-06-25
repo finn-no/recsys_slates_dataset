23502de9dc32a916d2ac3d4013a108a857588a5d
simen eide <simen@eide.ai>
2021-06-25 12:16:34 +0000

    update dataset module to use the new numpy files

--- code/dataset.py
+++ code/dataset.py
@@ -5,40 +5,38 @@ logging.basicConfig(format='%(asctime)s %(message)s', level='INFO')
 import pickle
 from torch.utils.data import Dataset, DataLoader
 import torch
-
+import json
 def mkdir(path):
     if os.path.isdir(path) == False:
         os.makedirs(path)
-
+import numpy as np
 #%% DATALOADERS
 class SequentialDataset(Dataset):
     '''
      Note: displayType has been uncommented for future easy implementation.
     '''
-    def __init__(self, data, sample_uniform_action=False):
+    def __init__(self, data, sample_uniform_slate=False):
 
         self.data = data
-        self.num_items = self.data['action'].max()+1
-        self.sample_uniform_action = sample_uniform_action
-        logging.info(f"Loading dataset with action size={self.data['action'].size()} and uniform candidate sampling={self.sample_uniform_action}")
+        self.num_items = self.data['slate'].max()+1
+        self.sample_uniform_slate = sample_uniform_slate
+        logging.info(f"Loading dataset with slate size={self.data['slate'].size()} and uniform candidate sampling={self.sample_uniform_slate}")
 
     def __getitem__(self, idx):
         batch = {key: val[idx] for key, val in self.data.items()}
 
-        if self.sample_uniform_action:
+        if self.sample_uniform_slate:
             # Sample actions uniformly:
-            action = torch.randint_like(batch['action'], low=3, high=self.num_items)
+            action = torch.randint_like(batch['slate'], low=3, high=self.num_items)
             
             # Add noclick action at pos0 
             # and the actual click action at pos 1 (unless noclick):
             action[:,0] = 1
             clicked = batch['click']!=1
             action[:,1][clicked] = batch['click'][clicked]
-            batch['action'] = action
+            batch['slate'] = action
             # Set click idx to 0 if noclick, and 1 otherwise:
             batch['click_idx'] = clicked.long()
-            
-            
             
         return batch
 
@@ -50,14 +48,15 @@ def load_dataloaders(data_dir,
                      batch_size=1024,
                      split_trainvalid=0.90,
                      t_testsplit = 5,
-                     sample_uniform_action=False):
+                     sample_uniform_slate=False):
 
     logging.info('Load data..')
-    data = torch.load(f'{data_dir}/data.pt')
-    dataset = SequentialDataset(data, sample_uniform_action)
+    with np.load(f'{data_dir}/data.npz') as data_np:
+        data = {key: torch.tensor(val) for key, val in data_np.items()}
+    dataset = SequentialDataset(data, sample_uniform_slate)
     
-    with open(f'{data_dir}/ind2val.pickle', 'rb') as handle:
-        ind2val = pickle.load(handle)
+    with open(f'{data_dir}/ind2val.json', 'rb') as handle:
+        ind2val = json.load(handle)
 
     num_validusers = int(len(dataset) * (1-split_trainvalid)/2)
     num_testusers = int(len(dataset) * (1-split_trainvalid)/2)
@@ -79,7 +78,7 @@ def load_dataloaders(data_dir,
         }
 
     dataloaders = {
-        phase: DataLoader(ds, batch_size=batch_size, shuffle=True)
+        phase: DataLoader(ds, batch_size=batch_size, shuffle=(phase=="train"), num_workers=12)
         for phase, ds in subsets.items()
     }
     for key, dl in dataloaders.items():
@@ -87,8 +86,7 @@ def load_dataloaders(data_dir,
             f"In {key}: num_users: {len(dl.dataset)}, num_batches: {len(dl)}"
         )
 
-
-    with open(f'{data_dir}/itemattr.pickle', 'rb') as handle:
-        itemattr = pickle.load(handle)
+    with np.load(f'{data_dir}/itemattr.npz', mmap_mode=None) as itemattr_file:
+        itemattr = {key : val for key, val in itemattr_file.items()}
 
     return ind2val, itemattr, dataloaders
